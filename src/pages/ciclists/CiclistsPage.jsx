f262a764b97c68581ad4d24938f26e80acee9e7a
sebastian2000pk <juochoas@unal.edu.co>
2022-06-24 21:41:51 -0500

    item

--- src/pages/ciclists/CiclistsPage.jsx
+++ src/pages/ciclists/CiclistsPage.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import "./ciclistsPage.css";
 
 // Components
 import Item from "../../components/item/Item";
 
 const CiclistsPage = () => {
-  const ciclists = [1, 2, 3, 4, 5];
+  const ciclists = [
+    { name: "ciclista 1" },
+    { name: "ciclista 2" },
+    { name: "ciclista 3" },
+    { name: "ciclista 4" },
+    { name: "ciclista 5" },
+  ];
 
   return (
     <div className="ciclists-page">
-      {ciclists.map((ciclist) => (
-        <Item>
-          <div>test</div>
-        </Item>
-      ))}
+      <div className="list-ciclists">
+        {ciclists.map((ciclist) => (
+          <Item>
+            <div>{ciclist.name}</div>
+          </Item>
+        ))}
+      </div>
     </div>
   );
 };
