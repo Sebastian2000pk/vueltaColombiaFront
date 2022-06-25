3320a2033b0be404c4d94ed3b52348fd7922655d
sebastian2000pk <juochoas@unal.edu.co>
2022-06-25 18:13:38 -0500

    navbar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navigation.css";
-import Logo from '../../images/unal-logo.png';
+import Logo from "../../images/unal-logo.png";
 
 const Navigation = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
     <>
-      <div className="navbar">
-        <Link to="#" className="navbar-button">
-          []
-        </Link>
-      </div>
       <nav className={navbar ? "nav-menu open" : "nav-menu"}>
+        <div className="navbar-button" onClick={() => setNavbar(!navbar)}>
+          <div className={navbar ? "button-hamburguer activate" : "button-hamburguer"}>
+            <span className="top-bun"></span>
+            <span className="suffing"></span>
+            <span className="botton-bun"></span>
+          </div>
+        </div>
         <div className="container-logo">
-          <img  src={Logo} alt="unal logo"/>
+          <img src={Logo} alt="unal logo" />
         </div>
         <ul className="nav-menu--list">
           <li className="navbar-item">
